02d94f8c0aff57de326e73b6af442a18241fb2f4
Aman Takhar <amantakhar@Amans-MacBook-Air-2.local>
2026-02-12 17:15:02 +0530

    first completed version

--- ToDoApp/urls.py
+++ ToDoApp/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('addTask/',views.addTask,name='addTask')
+    path('addTask/',views.addTask,name='addTask'),
+    path('markAsDone/<int:pk>/',views.markAsDone,name='markAsDone'),
+    path('markAsUnDone/<int:pk>/',views.markAsUnDone,name='markAsUnDone'),
+    path('deleteTask/<int:pk>',views.deleteTask,name='deleteTask'),
+    path('editTask/<int:pk>',views.editTask,name='editTask'),
 ]
--- ToDoApp/views.py
+++ ToDoApp/views.py
@@ -7,9 +7,11 @@ from .models import tasks
 
 # Create your views here.
 def home(request):
-    task = tasks.objects.filter(is_completed = False)
+    task = tasks.objects.filter(is_completed = False).order_by('-updated_at')
+    completed_task = tasks.objects.filter(is_completed = True).order_by('-updated_at')
     context = {
-        'tasks': task
+        'tasks': task,
+        'completed_task':completed_task,
     }
     return render(request,'index.html',context)
 
@@ -17,3 +19,31 @@ def addTask(request):
     task = request.POST['task']
     tasks.objects.create(task_name = task)
     return redirect('home')
+
+def markAsDone(request,pk):
+    task = get_object_or_404(tasks,pk=pk)
+    task.is_completed = True
+    task.save()
+    return redirect('home')
+
+def markAsUnDone(request,pk):
+    task = get_object_or_404(tasks,pk=pk)
+    task.is_completed = False
+    task.save()
+    return redirect('home')
+
+def deleteTask(request,pk):
+    task = get_object_or_404(tasks,pk=pk)
+    task.delete()
+    return redirect('home')
+
+def editTask(request,pk):
+    task = get_object_or_404(tasks,pk=pk)
+    if(request.method == 'POST'):
+        new_task = request.POST['new_task']
+        task.task_name = new_task
+        task.save()
+        return redirect('home')
+    else:
+        return render(request,'editTask.html',{'task':task})
+
